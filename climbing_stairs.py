424b0dbe59e66416815a303b9287c2e26c09ea08
Branden-007 <brandenmac25@gmail.com>
2025-07-09 14:52:25 +0530

    climbing stairs

---
+++ climbing_stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def climbStairs(self, n):
+        hashMap = {}   
+        
+        def climb(n):
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            if n in hashMap:
+                return hashMap[n]
+            hashMap[n] = climb(n-1) + climb(n-2)
+            return hashMap[n]
+        
+        return climb(n)
+            
