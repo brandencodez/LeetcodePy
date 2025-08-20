1dc571fccfddc396fa309637f2314769a7166fbb
Branden-007 <brandenmac25@gmail.com>
2025-08-20 16:52:27 +0530

    Updated solution score-of-string

--- strings/score-of-a-string.py
+++ strings/score-of-a-string.py
@@ -1,13 +1,6 @@
-def scoreOfString(self, s: str) -> int:
-        sum = 0
-
-        for ptr1 in range(len(s)-1):
-             ptr2 = ptr1+1
-             val_1 = s[ptr1]
-             val_2 = s[ptr2]
-
-             asci_val1 = ord(val_1)
-             asci_val2 = ord(val_2)
-             result = abs(asci_val1 - asci_val2)
-             sum+= result
-        return sum
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        total = 0 
+        for i in range(len(s)-1):
+            total+= abs(ord(s[i]) - ord(s[i+1]))
+        return total 
